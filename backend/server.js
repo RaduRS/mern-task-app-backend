169044204d23502041d90c017631185d680df0d3
RaduRS <rsrusu90@gmail.com>
2023-02-06 17:11:22 +0000

    update cors

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: [
-      "https://mern-task-app-omn9.onrender.com",
-      "https://www.tasky.radu-rusu.com",
-      "https://tasky.radu-rusu.com",
-    ],
+    origin: "*"
   })
 );
 app.use("/api/tasks", taskRoutes);
